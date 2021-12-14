6bdcffe99497629a08e6acd961bcef3360f26a6c
S-Spence <56896848+S-Spence@users.noreply.github.com>
2021-12-13 22:39:04 -0500

    Updating comments

--- src/test/java/tests.java
+++ src/test/java/tests.java
@@ -3,12 +3,12 @@ package test.java;
 import main.*;
 
 public class tests {
-   // Test that it will not accept more than one character for symbol
    // Test set symbol
    // Test set type
    // Test set label
    // Test set row 
-   // Test set height -> test negative value 
-   // Test that it will not set a label longer than width on that row
-   
+   // Test set height
+   // Test print triangle -> label -> default
+   // Test print square -> label -> default
+   // Test print diamond -> label -> default  
 }
